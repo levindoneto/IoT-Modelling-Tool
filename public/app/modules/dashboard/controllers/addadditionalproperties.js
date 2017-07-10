d93d518a15dc07152c7acef999ba27fa5aac82bb
Levindo Neto <levindogtn@gmail.com>
2017-07-10 23:38:42 +0200

    Modify adding of new properties on the devices/components

--- public/app/modules/dashboard/controllers/addadditionalproperties.js
+++ public/app/modules/dashboard/controllers/addadditionalproperties.js
@@ -24,10 +24,60 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash, $fireb
     /* Function to get the key on an object by a value on this object
      */
     $scope.keyByValue = function(value) {
-        var kArray = Object.keys(greetings); // Creating array of keys
-        var vArray = Object.values(greetings); // Creating array of values
+        let typeDB;
+        console.log("Value:::::", value,"::::::");
+
+        if (value === "Sensor") {
+            console.log("ASSHOLE");
+        }
+
+        switch (value) {
+            case "Device":
+                typeDB = "Device";
+            case "Sensor":
+                typeDB = "SensingDevice";
+            case "Actuator":
+                typeDB = "ActuatingDevice";
+            default:
+                typeDB = "";
+        }
+        var kArray = Object.keys(types); // Creating array of keys
+        var vArray = Object.values(types); // Creating array of values
         var vIndex = vArray.indexOf(value);  // Finding value index 
-        return kArray[vIndex];                      // Returning key by value index
+        console.log(typeDB);
+        return typeDB;                      // Returning key by value index
     };
+
+    /* Adding data on the database (One property:value on a key per time)
+     * context->context_key->new_property = new_property_value
+     * On the Front-End: ng-click="addspecificproperty(new_property, new_property_value, context_key)
+     */
+    $scope.addspecificproperty = function (context_key, new_property, new_property_value) {
+        console.log("THE KEY: ",context_key);
+        var ref = firebase.database().ref('models/'+context_key); // Accessing context->context_key on the database
+        let auxObjContext = {}; // Auxiliar to add a key:value on a specific object
+        auxObjContext[new_property] = new_property_value; // In this way just a key with a value is added, not a new object
+        ref.update(auxObjContext); // Updating the object on the database
+        console.log("add the value: ", new_property_value);
+        swal({
+            title: "The new property and its value have been added with success!",
+            timer: 1700,
+            showConfirmButton: false
+        });
+    };
+    
+    /* Function to emulate the for i in range with AngularJS 
+     * for (min, max, step) {
+     *     do something;
+     * }
+     */
+    $scope.range = function(min, max, step) {
+        step = step || 1;
+        var input = [];
+        for (var i = min; i <= max; i += step) {
+            input.push(i);
+        }
+    return input;
+    }
     
 }]);
