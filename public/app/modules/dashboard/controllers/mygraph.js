7d8220b0d1a2ef15f8b47471cc947ba3cf4922b1
Levindo Neto <levindogtn@gmail.com>
2017-07-18 14:35:49 +0200

    Add function for getting the graphs from the database and make them accessible from the view

--- public/app/modules/dashboard/controllers/mygraph.js
+++ public/app/modules/dashboard/controllers/mygraph.js
@@ -1,7 +1,27 @@
 
 dashboard.controller("mygraphController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash','$firebaseObject','$firebaseArray',
 function ($rootScope, $scope, $state, $location, dashboardService, Flash, $firebaseObject, $firebaseArray) {
-    //TODO
     var vm = this; //controllerAs
-    return true;
+    const default_graphProps = [];
+    var ref = firebase.database().ref('graphs/'); // Loading all the graphs from the database
+    var graphList = $firebaseArray(ref);
+
+    /* Loading data from the database */
+    graphList.$loaded().then(function(){
+          $scope.graphs = graphList; // scope.graph = database->graph 
+    });
+
+    /* Function responsible for passing the selected graph to the scope */
+    $scope.modal = function (keySelGraph) {
+        console.log("KEY GRAPH:::: ", keySelGraph);
+        var ref = firebase.database().ref('graphs/'+keySelGraph);
+        var graphObj = $firebaseObject(ref);
+        graphObj.$loaded().then(function(){ //Loading graphs from the database as an object
+            $scope.modelgraph = graphObj;
+            console.log("THE VALUE::::::", graphObj);
+        });
+    };
+    
+
+
 }]);
