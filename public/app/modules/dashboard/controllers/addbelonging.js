e94890508215b572818789d969082d09cca01213
Levindo Neto <levindogtn@gmail.com>
2017-09-19 18:37:59 +0200

    Add new sync structure for devices and components

--- public/app/modules/dashboard/controllers/addbelonging.js
+++ public/app/modules/dashboard/controllers/addbelonging.js
@@ -3,24 +3,37 @@ dashboard.controller("addbelongingController", ['$rootScope', '$scope', '$state'
 function ($rootScope, $scope, $state, $location, dashboardService, Flash, $firebaseObject, $firebaseArray, Upload, $timeout, notification) {
     const vm = this;
 
-    vm.addbelonging = function (model, file) {
+    vm.addbelonging = function (prefix, type, model, file) { // prefix->type->model
         Upload.base64DataUrl(file).then((base64Url) => {
             model.userUid = $rootScope.userDB.uid;
             const refImages = firebase.database().ref('images/');
             const imageList = $firebaseArray(refImages);
+            const auxModel = {};
+            const auxType = {};
+            const modelKeys = model;
+
             imageList.$loaded().then(() => {
                 imageList.$add(base64Url).then((imref) => {
                     //console.log("imref");
                     //console.log(imref)
                     model.imageFile = imref.key;
-
                     const ref = firebase.database().ref('models/');
+                    const refDevComp = firebase.database().ref('devComp/');
                     const modelList = $firebaseArray(ref);
+                    const devCompList = $firebaseArray(refDevComp);
+                    modelKeys.prefixCompany = prefix;
+                    modelKeys.type = type;
+                    
                     /* $loaded function:
                      * Returns a promise which is resolved when the initial object data has been 
                      * downloaded from the database. The promise resolves to the $firebaseObject itself.
                      */
                     modelList.$loaded().then(() => {
+                        console.log('MODEL: ', model);
+                        auxType[type] = model;
+                        auxModel[prefix] = auxType; // [prefix][type] can't be accessed on the fly
+                        
+                        console.log('AUX MODEL: ', auxModel);
                         /* $add function:
                          * Creates a new record in the database and adds the record to our 
                          * local synchronized array.
@@ -28,14 +41,21 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash, $fireb
                          * saved to the server. The promise resolves to the Firebase reference 
                          * for the newly added record, providing easy access to its key.
                          */
-                        modelList.$add(model).then((ref) => {
-                            swal({
-                                title: "It's been added with sucess!",
-                                timer: 1700,
-                                showConfirmButton: false
-                            });
+                        swal({
+                            title: "It's been added with sucess!",
+                            timer: 1700,
+                            showConfirmButton: false
+                        });
+                        console.log('im here');
+                        modelList.$add(modelKeys).then((ref) => {
                         });
                     });
+                    
+                    devCompList.$loaded().then(() => {
+                        devCompList.$add(auxModel).then((refDevComp) => {
+                        });
+                    });
+                    
                 });
             });
         });
