66aaa8505123699ecfbd6023f789cb8746b22ab1
Levindo Neto <levindogtn@gmail.com>
2017-10-01 16:14:59 +0200

    Modify creation of new type on the database

--- public/app/modules/dashboard/controllers/addbelonging.js
+++ public/app/modules/dashboard/controllers/addbelonging.js
@@ -2,7 +2,10 @@
 dashboard.controller("addbelongingController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash','$firebaseObject','$firebaseArray','Upload','$timeout','notification',
 function ($rootScope, $scope, $state, $location, dashboardService, Flash, $firebaseObject, $firebaseArray, Upload, $timeout, notification) {
     const vm = this;
-
+    function getOut() {
+        console.log('Getting out...');
+        return -1;
+    }
     vm.addbelonging = function (prefix, type, model, file) { // prefix->type->model
         Upload.base64DataUrl(file).then((base64Url) => {
             model.userUid = $rootScope.userDB.uid;
@@ -11,6 +14,8 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash, $fireb
             const auxModel = {};
             const auxType = {};
             const modelKeys = model;
+            const auxInfo = {};
+            var auxBind = {};
             var auxPrefix = {};
             imageList.$loaded().then(() => {
                 imageList.$add(base64Url).then((imref) => {
@@ -20,32 +25,40 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash, $fireb
                     const ref = firebase.database().ref('models/');
                     const refDevComp = firebase.database().ref('devComp/');
                     const modelList = $firebaseArray(ref);
-                    
+                    let debug = 0;
                     refDevComp.on("value", (snapshot) => { // The whole object savedModels with all the saved models
-                        console.log('prefix.toUpperCase(): ', prefix.toUpperCase());
-                        console.log('snapshot.val(): ', snapshot.val());
+                        console.log('DEBUG (0): ', debug);
+                        //console.log('prefix.toUpperCase(): ', prefix.toUpperCase());
+                        //console.log('snapshot.val(): ', snapshot.val());
                         if (prefix.toUpperCase() in snapshot.val()) {
                             if (type in snapshot.val()[prefix]) {
-                                auxPrefix = snapshot.val();
-                                auxPrefix[prefix][type] = model;
-                                refDevComp.update(auxPrefix);
+                                auxInfo[(Object.keys(snapshot.val()[prefix][type]).length).toString()] = model;
+                                auxType[type] = '';
+                                auxBind[prefix] = auxType;
+                                auxBind[prefix][type] = Object.assign(auxInfo, snapshot.val()[prefix][type]);              
+                                //auxPrefix = snapshot.val();
+                                //auxPrefix[prefix][type] = auxInfo;
+                                refDevComp.update(auxBind[prefix][type]);
                                 return;
                             }
                             else {
-                                //console.log('CREATE TYPE');
-                                auxPrefix = snapshot.val();
-                                auxPrefix[prefix][type] = model;
+                                console.log('CREATE TYPE');
+                                auxInfo['0'] = model; // First model of the just created type
+                                auxType[type] = auxInfo;
+                                auxPrefix[prefix] = Object.assign(snapshot.val()[prefix], auxType);
                                 refDevComp.update(auxPrefix);
                                 return;
                             }
                         }
                         else {
-                            //console.log('CREATE PREFIX');
+                            console.log('CREATE PREFIX');
+                            debug += 1;
+                            console.log('DEBUG ELSE (1): ', debug);
                             auxType[type] = model;
                             auxPrefix[prefix] = auxType;
-                            refDevComp.update(auxPrefix);
-                            
-                            return;
+                            refDevComp.update(auxPrefix);  
+                            getOut(); 
+                            return; 
                         }
                     });
                     
@@ -58,11 +71,11 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash, $fireb
                      * downloaded from the database. The promise resolves to the $firebaseObject itself.
                      */
                     modelList.$loaded().then(() => {
-                        console.log('MODEL: ', model);
+                        //console.log('MODEL: ', model);
                         auxType[type] = model;
                         auxModel[prefix] = auxType; // [prefix][type] can't be accessed on the fly
                         
-                        console.log('AUX MODEL: ', auxModel);
+                        //console.log('AUX MODEL: ', auxModel);
                         /* $add function:
                          * Creates a new record in the database and adds the record to our 
                          * local synchronized array.
@@ -75,7 +88,7 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash, $fireb
                             timer: 1700,
                             showConfirmButton: false
                         });
-                        console.log('im here');
+                        //console.log('im here');
                         modelList.$add(modelKeys).then((ref) => {
                         });
                     });
