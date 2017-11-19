a3f2b125092d1f08500e5dbeed0ea07520a18f64
Levindo Neto <levindogtn@gmail.com>
2017-11-19 04:04:43 +0100

    Fix the use of the map between components and specific types

--- public/app/modules/dashboard/digital_environment/src/main/js/src/js/components/NavigationBar.js
+++ public/app/modules/dashboard/digital_environment/src/main/js/src/js/components/NavigationBar.js
@@ -155,71 +155,40 @@ export default class NavigationBar extends React.Component {
     };
 
     bind = () => {
-        //Add sensor
-
-
-        /*
-        //Add adapter
-        var jsonData = {
-            "name": "dummyAdapter",
-            "description": "An adapter for tests",
-            "service": {
-              "name": "service-stub.conf",
-              "content": "stub conf"
-            },
-            "routines": [{
-              "name": "service-stub.py",
-              "content": "service code"
-            }]
-          };
-        $.ajax({
-            type: "POST",
-            url: "http://192.168.209.176:8080/MBP/api/types/ ",
-            contentType: 'application/json;charset=UTF-8',
-            data: JSON.stringify(jsonData)
-        }).done((msg) => {
-           console.log('The type has been posted');
-        });
-        */
-
         const refSavedModels = firebase.database().ref('savedModels/');
         const refInfoSaved = firebase.database().ref('infoSavedModels'); // For getting the current loaded model
         const refDevsWithSubsystems = firebase.database().ref('devicesWithSubsystems');
         const refMapTypeComponents = firebase.database().ref('mapTypeComponents');
         const auxSavedModels = {};
         let idSplit;
-        let idComp;
-        var componentType;
-        refInfoSaved.on("value", (snapshot) => {
-            /* Get the type from the map component:specificType */
-            auxSavedModels[snapshot.val().lastLoadedModel] = JSON.stringify(DeviceStore.getModel());
-            refSavedModels.update(auxSavedModels);
-            backend.fire_ajax_save(snapshot.val().lastLoadedModel, DeviceStore.getModel());
+        let mapTypeComp;
 
-            // Register the devices on MBD ::: TEST 
-            refDevsWithSubsystems.on("value", (snapdev) => { // Listener on devices with sensors/actuators (whole element)
-                for (var i in snapdev.val()[snapshot.val().lastLoadedModel]) { // Access devices from the current loaded model
-                    console.log('Register the device <', i, '>');
-                    for (var j in snapdev.val()[snapshot.val().lastLoadedModel][i]) {
-                        idSplit = (snapdev.val()[snapshot.val().lastLoadedModel][i][j][Object.keys(snapdev.val()[snapshot.val().lastLoadedModel][i][j])[0]]['@type']).split(':'); // Get the id of the component without the prefix
-                        refMapTypeComponents.on("value", (compType) => { 
-                            componentType = compType.val()[idSplit[1].toString()]; // The the type of the component by the id
-                        });
-                        setTimeout(() => {
-                            backend.bindComponent(Object.keys(snapdev.val()[snapshot.val().lastLoadedModel][i][j])[0], componentType, '5a0f2a8b4f0c7363179e58e5','5a0f17a64f0c7363179e58da', 'http://192.168.209.176:8080/MBP'); // Post component into the MBD platform
-                        }, 2500);
-                        console.log('Register the componenent <', snapdev.val()[snapshot.val().lastLoadedModel][i][j][Object.keys(snapdev.val()[snapshot.val().lastLoadedModel][i][j])[0]]['@type'], '> as subsystem of the device <', i, '>'); //POST /api/types/ HTTP/1.1
-                    }
-                }
-                //keycomp: 
-            });
+        // Get the map between components and types
+        refMapTypeComponents.on("value", (map) => {
+            mapTypeComp = map.val();
         });
+        setTimeout(() => {
+            refInfoSaved.on("value", (snapshot) => {
+                auxSavedModels[snapshot.val().lastLoadedModel] = JSON.stringify(DeviceStore.getModel());
+                refSavedModels.update(auxSavedModels);
+                backend.fire_ajax_save(snapshot.val().lastLoadedModel, DeviceStore.getModel());
+                refDevsWithSubsystems.on("value", (snapdev) => { // Listener on devices with sensors/actuators (whole element)
+                    for (var i in snapdev.val()[snapshot.val().lastLoadedModel]) { // Access devices from the current loaded model
+                        //console.log('Register the device <', i, '>');
+                        for (var j in snapdev.val()[snapshot.val().lastLoadedModel][i]) {
+                            idSplit = (snapdev.val()[snapshot.val().lastLoadedModel][i][j][Object.keys(snapdev.val()[snapshot.val().lastLoadedModel][i][j])[0]]['@type']).split(':'); // Get the id of the component without the prefix (0: prefix, 1:id)                    
+                            backend.bindComponent(Object.keys(snapdev.val()[snapshot.val().lastLoadedModel][i][j])[0], mapTypeComp[idSplit[1]], '5a0f2a8b4f0c7363179e58e5','5a0f17a64f0c7363179e58da', 'http://192.168.209.176:8080/MBP'); // Post component into the MBD platform
+                            //console.log('Register the componenent <', snapdev.val()[snapshot.val().lastLoadedModel][i][j][Object.keys(snapdev.val()[snapshot.val().lastLoadedModel][i][j])[0]]['@type'], '> as subsystem of the device <', i, '>'); //POST /api/types/ HTTP/1.1
+                        }
+                    }
+                });
+            });
+        }, 2000);
         swal({
             title: 'The model has been saved and bound successfully',
             timer: 1500,
             showConfirmButton: false
-        });
-        
+        });     
     };
 
     handleOpenHelp = () => {
