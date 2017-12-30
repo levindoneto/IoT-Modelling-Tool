a5e09c44d09fe00cec2de93bb619d0c99d7f77ba
Levindo Neto <levindogtn@gmail.com>
2017-12-30 22:20:46 +0100

    Add is admin information in the registration of new users

--- public/app/modules/login/loginCtrl.js
+++ public/app/modules/login/loginCtrl.js
@@ -1,54 +1,45 @@
 
-login.controller("loginCtrl", ['$rootScope', '$scope', '$state', '$location', 'loginService', 'Flash','apiService','$firebaseAuth','$firebaseObject','$firebaseArray',
+login.controller('loginCtrl', ['$rootScope', '$scope', '$state', '$location', 'loginService', 'Flash','apiService','$firebaseAuth','$firebaseObject','$firebaseArray',
 function ($rootScope, $scope, $state, $location, loginService, Flash, apiService, $firebaseAuth, $firebaseObject, $firebaseArray) {
     var vm = this;
+    var auth = $firebaseAuth();
     vm.getUser = {};
     vm.setUser = {};
     vm.signIn = true;
-    var auth = $firebaseAuth();
     //access login
     vm.login = function (data) {
-        console.log(data)
         auth.$signInWithEmailAndPassword(data.Email, data.Password).then((firebaseUser) => {
-            //console.log('Signed in as:', firebaseUser.uid);
         }).catch((error) => {
-            Flash.create('Danger: Fail in the autentication->' + error, 'large-text');
+            Flash.create(`Danger: Fail in the autentication->${error}`, 'large-text');
         });
-
     };
 
     //get registration details
     vm.register = function () {
-            var refUsers = firebase.database().ref('users/');
-            var userList = $firebaseArray(refUsers);
-            var alreadyExist = false;
+        var refUsers = firebase.database().ref('users/');
+        var userList = $firebaseArray(refUsers);
+        var alreadyExist = false;
 
-            userList.$loaded().then(() => {
-                if(!alreadyExist)
-                auth.$createUserWithEmailAndPassword(vm.setUser.Email, vm.setUser.Password)
-                .then(function(firebaseUser) {
-
-                    var ref = firebase.database().ref('users/'+firebaseUser.uid);
-                    var obj = $firebaseObject(ref);
-
-                    obj.$bindTo($rootScope, "user").then(function() {
-                        console.log($rootScope.user);
-                        $rootScope.user = vm.setUser;
-                    });
+        userList.$loaded().then(() => {
+            if(!alreadyExist)
+            auth.$createUserWithEmailAndPassword(vm.setUser.Email, vm.setUser.Password)
+            .then((firebaseUser) => {
+                var ref = firebase.database().ref(`users/${firebaseUser.uid}`);
+                var obj = $firebaseObject(ref);
+                obj.$bindTo($rootScope, 'user').then(() => {
+                    vm.setUser.isAdmin = Boolean(false | vm.setUser.isAdmin);
+                    $rootScope.user = vm.setUser;
+                });
                 }).catch((error) => {
-                    Flash.create('Danger: Error with the register ->' + error, 'large-text');
+                    Flash.create(`Danger: Error with the register ->${error}`, 'large-text');
                 });
             }
         );
-
     };
 
     auth.$onAuthStateChanged((firebaseUser) => {
         if (firebaseUser) {
             $state.go('app.myaccount'); // Go to my account when the user is allowed to access the platform
-            //console.log('User ' + firebaseUser.uid + ' created successfully!');
-        } else {
-            //console.log('Signed out');
-        }
+        } 
     });
 }]);
