7b31a0dbd3fa8b39792429c44a58ca312272e12d
Levindo Neto <levindogtn@gmail.com>
2018-01-13 15:10:36 +0100

    Modify hover effects throughout the app and margins on the My Account page

--- public/app/modules/dashboard/controllers/myaccount.js
+++ public/app/modules/dashboard/controllers/myaccount.js
@@ -1,33 +1,24 @@
 dashboard.controller('myaccountController', ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash', '$firebaseArray','$firebaseAuth','$firebaseObject',
 function ($rootScope, $scope, $state, $location, dashboardService, Flash, $firebaseArray, $firebaseAuth, $firebaseObject) {
-    var vm = this;
-    var ref = firebase.database().ref('defaults/defaultcontext'); // Acces the object context selected by the user
-    var refg = firebase.database().ref('defaults/defaultgraph');
-    var contextDefaultObj = $firebaseObject(ref); // Access the default @context key
-    var refContexts = firebase.database().ref('contexts/'); // Access the object context selected by the user
-    var allContexts = $firebaseObject(refContexts);
-    var refGraphs = firebase.database().ref('graphs/'); // Access the object @graphs from Firebase
-    var allGraphs = $firebaseObject(refGraphs);
-    var graphDefaultObj = $firebaseObject(refg); // Acess the default @graph key
-
+    const vm = this;
+    const ref = firebase.database().ref('defaults/defaultcontext'); // Acces the object context selected by the user
+    const refg = firebase.database().ref('defaults/defaultgraph');
+    const contextDefaultObj = $firebaseObject(ref); // Access the default @context key
+    const refContexts = firebase.database().ref('contexts/'); // Access the object context selected by the user
+    const allContexts = $firebaseObject(refContexts);
+    const refGraphs = firebase.database().ref('graphs/'); // Access the object @graphs from Firebase
+    const allGraphs = $firebaseObject(refGraphs);
+    const graphDefaultObj = $firebaseObject(refg); // Acess the default @graph key
 
     setTimeout(() => { // It works as a promise without using any function as parameter
-            try {
-                const currentKey = contextDefaultObj.$value.toString();
-            } catch (err) {
-                console.log('The default context has not been obtained yet.\n', err);
-            }
-            $scope.currentDefaultContext = allContexts[currentKey.toString()].idcontext.toString();
-        }, 2000);
+        let current_key = contextDefaultObj.$value.toString();
+        $scope.currentDefaultContext = allContexts[current_key.toString()].idcontext.toString();
+    }, 2000);
 
-    setTimeout(() => { // setTimeout(function() { 
-        try {
-            const currentKeyGraph = graphDefaultObj.$value.toString();
-        } catch (err) {
-            console.log('The default graph has not been obtained yet.\n', err);
-        }
-            $scope.currentDefaultGraph = allGraphs[currentKeyGraph.toString()].idgraph.toString();
-        }, 2000);
+setTimeout(() => { // setTimeout(function() { 
+        let current_key_graph = graphDefaultObj.$value.toString();
+        $scope.currentDefaultGraph = allGraphs[current_key_graph.toString()].idgraph.toString();
+    }, 2000);
     
 
     $scope.showAccountinfo = function (user) {
